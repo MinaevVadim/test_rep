0c240b4fa26da48c7bf7230cbaede53fccddae9f
Vadim <96593215+MinaevVadim@users.noreply.github.com>
2025-05-30 00:11:54 +0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,15 +2,9 @@ def generator():
     for i in range(101111):
         yield i
 
-<<<<<<< HEAD
-#changes commit
-
-#remote generator changes
-=======
 #changes commit!
 
 #remote generator changes!
->>>>>>> a013321b4e5cfccdca93092e3b6e6765e2215151
 
 if __name__ == "__main__":
     for i in generator():
