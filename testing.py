9b2f478bde04f1f61042855f2ea7486817ed1c0c
MinaevVadim <minaev.vad@yandex.ru>
2025-05-31 13:59:16 +0300

    git add .

--- testing.py
+++ testing.py
@@ -2,19 +2,9 @@ def generator():
     for i in range(101111):
         yield i
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-#changes commit!
-=======
 #changes commit!!!~~
->>>>>>> generator
 
 #remote generator changes!!!~~
-=======
-#changes commit!!!~~~
-
-#remote generator changes!!!~~~
->>>>>>> 7c4f9b19df88759b40a24a9052dcb4a631927571
 
 if __name__ == "__main__":
     for i in generator():
